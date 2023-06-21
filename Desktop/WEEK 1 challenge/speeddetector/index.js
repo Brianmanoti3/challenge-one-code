2550d18622dbef300708fafa57933e161b05ef72
Brian Manoti <brian.manoti@student.moringaschool.com>
2023-06-21 09:39:27 +0300

    complete assignment

---
+++ Desktop/WEEK 1 challenge/speeddetector/index.js
@@ -0,0 +1,18 @@
+//calculating the speed of a car
+function checkspeed() {
+    const speed = parseFloat(prompt("Enter the car speed"));
+
+    if (speed <70){
+        return "OK";
+    }else{
+        const demeritpoints = Math.floor((speed - 70)/ 5);
+
+        if (demeritpoints > 12){
+            return "License suspended";
+        }else {
+            return "points: " + demeritpoints;
+            }
+        }
+    }
+    
+    console.log(checkspeed());//call function 
